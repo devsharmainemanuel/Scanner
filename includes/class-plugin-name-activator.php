d5d32be79ca0fa352435bf2b3e6e36c756e27760
sharmaine.manuel <sharmaine.manuel@intelligentoutsourcing.co.uk>
2018-09-05 16:03:19 +0800

    initial commit

---
+++ includes/class-plugin-name-activator.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/includes
+ * @author     Your Name <email@example.com>
+ */
+class Plugin_Name_Activator {
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+		// 
+	}
+
+	public static function add_token()
+	{
+		if(!get_option('_helio_wp_manager_token')) {
+			add_option('_helio_wp_manager_token', sha1(md5((uniqid()))));
+		}
+	}
+}
